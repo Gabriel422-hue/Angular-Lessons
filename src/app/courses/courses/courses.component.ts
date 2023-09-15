789ae8af958536a93789db793c3b2f920ee5d448
Gabriel Victor <gabrielvictor422@hotmail.com>
2023-09-15 16:09:14 -0300

    Atualizações Das Aulas

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
+
+  courses: Course[] = [];
+  displayedColumns = ['name', 'category']
+
+
+  constructor(private coursesService: CoursesService){
+
+  }
+
+  ngOnInit():void{
+    this.courses = this.coursesService.list();
+  }
+
 
 }
